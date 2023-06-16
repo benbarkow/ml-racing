ce1c4596eb2838a67fde9e41760b718b7ae5e799
Ben Barkow <benbarkow02@gmail.com>
2023-06-16 21:15:16 +0200

    added cuda option

--- ML/tb_test.py
+++ ML/tb_test.py
@@ -5,3 +5,7 @@ tb = program.TensorBoard()
 tb.configure(argv=[None, '--logdir', config.tb_logs])
 url = tb.launch()
 print(f"Tensorflow listening on {url}")
+
+# keep the program running
+while True:
+    pass
