02ca0a820f0cab0a4b52c807d31deaf5d5440cab
Ben Barkow <benbarkow02@gmail.com>
2023-07-27 14:13:22 +0200

    docker compose file fixed

--- ros/ros2_ws/src/car_camera/car_camera/receiver.py
+++ ros/ros2_ws/src/car_camera/car_camera/receiver.py
@@ -5,20 +5,22 @@ from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Image
 import cv2 # OpenCV library
  
 class CarCameraReciever(Node):
-  def __init__(self):
-    super().__init__('image_subscriber')
-    self.subscription = self.create_subscription(
-      Image, 
-      'car_video_frames', 
-      self.listener_callback, 
-      10)
-    self.subscription # prevent unused variable warning
-    self.br = CvBridge()
+	def __init__(self):
+		super().__init__('image_subscriber')
+		self.subscription = self.create_subscription(
+		Image, 
+		'car_video_frames', 
+		self.listener_callback, 
+		10)
+		self.subscription # prevent unused variable warning
+		self.br = CvBridge()
    
-  def listener_callback(self, data):
-    current_frame = self.br.imgmsg_to_cv2(data)
-    cv2.imshow("car_camera", current_frame)
-    cv2.waitKey(1)
+	def listener_callback(self, data):
+		current_frame = self.br.imgmsg_to_cv2(data)
+		#log image as array
+		self.get_logger().info(str(current_frame))
+		# cv2.imshow("car_camera", current_frame)
+		# cv2.waitKey(1)
   
 def main(args=None):
   rclpy.init(args=args)
