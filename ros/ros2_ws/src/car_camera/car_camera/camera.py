c19e6ba3a6ea8ded32e9d71826a24c18b56117f1
Ben Barkow <benbarkow02@gmail.com>
2023-07-27 14:18:24 +0200

    resize image before pub

--- ros/ros2_ws/src/car_camera/car_camera/camera.py
+++ ros/ros2_ws/src/car_camera/car_camera/camera.py
@@ -5,21 +5,22 @@ from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Image
 import cv2 # OpenCV library
  
 class CarCameraPublisher(Node):
-  def __init__(self):
-    super().__init__('car_camera_pub')
-    self.publisher_ = self.create_publisher(Image, 'car_video_frames', 10)
-    timer_period = 0.03  # seconds
-    self.timer = self.create_timer(timer_period, self.timer_callback)
-    self.cap = cv2.VideoCapture(0)
-    self.br = CvBridge()
-   
-  def timer_callback(self):
-    ret, frame = self.cap.read()
-          
-    if ret == True:
-      self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
-    else:
-      self.get_logger().info('Cannot read camera frame')
+	def __init__(self):
+		super().__init__('car_camera_pub')
+		self.publisher_ = self.create_publisher(Image, 'car_video_frames', 10)
+		timer_period = 0.03  # seconds
+		self.timer = self.create_timer(timer_period, self.timer_callback)
+		self.cap = cv2.VideoCapture(0)
+		self.br = CvBridge()
+	
+	def timer_callback(self):
+		ret, frame = self.cap.read()
+		#resize image to 320x240
+		frame = cv2.resize(frame, (320, 240))
+		if ret == True:
+			self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+		else:
+			self.get_logger().info('Cannot read camera frame')
   
 def main(args=None):
   
