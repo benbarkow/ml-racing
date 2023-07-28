1af5f06237c567591bcf8e872ceb825cdae74f71
Ben Barkow <benbarkow02@gmail.com>
2023-07-28 11:11:53 +0200

    image only again

--- ML/ImageWrapper.py
+++ ML/ImageWrapper.py
@@ -7,6 +7,8 @@ from torchvision import transforms
 import torch
 from PIL import Image
 import time 
+import cv2
+
 
 class ImageWrapper(gym.ObservationWrapper):
 	def __init__(self, env):
@@ -14,18 +16,24 @@ class ImageWrapper(gym.ObservationWrapper):
 		# self.observation_space = Box(shape=(60,80,), low=0, high=255, dtype=np.uint8)
 		# self.observation_space = Tuple((Box(shape=(60,80,), low=0, high=255, dtype=np.uint8), Box(shape=(2,), low=-1, high=1, dtype=np.float32)));
 		self.observation_space = Dict({
-			"image1": Box(shape=(60,80,), low=0, high=255, dtype=np.uint8),
-			"vector": Box(shape=(2,), low=-1, high=1, dtype=np.float32)
+			"image": Box(shape=(60,80,), low=0, high=255, dtype=np.uint8),
+			# "vector": Box(shape=(2,), low=-1, high=1, dtype=np.float32)
 		})
 		# self.observation_space = Tuple((Box(shape=(512,), low=0, high=1, dtype=np.float32), Box(shape=(2,), low=-1, high=1, dtype=np.float32)));
 	def observation(self, obs):
-		# print("Observation received:", obs)
 		image = obs[0]
 		# rescale the image from 0-1 to 0-255 and convert to uint8
 		image = image * 255
 		image = image.astype(np.uint8)
-		#shape (60,80,1) to (60,80)
-		image = image.reshape((60,80))
+		#load as cv2 grayscale image
+		image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+		#gaussian blur
+		image = cv2.GaussianBlur(image, (5,5), 0)
+		#apply threshold
+		_, image = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
+		#convert to grayscale
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		#resize
+		image = cv2.resize(image, (80,60))
 
-		return {"image": image, "vector": obs[1]}
-
+		return {"image": image}
