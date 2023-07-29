3ec5bd8782f9972b3a74e11d2109b65a3f72b649
Ben Barkow <benbarkow02@gmail.com>
2023-07-30 00:27:36 +0200

    taining drift complete

--- ML/evaluate.py
+++ ML/evaluate.py
@@ -7,6 +7,7 @@ from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3 import PPO
+from ImageWrapper import ImageWrapper
 
 from FeatureExtractionWrapper import FeatureExtractionWrapper
 
@@ -19,9 +20,9 @@ if __name__ == '__main__':
     #init and start simulation
 	channel = EngineConfigurationChannel()
 	channel.set_configuration_parameters(time_scale=1.0)
-	unity_env = UnityEnvironment(file_name="build/ml-racing-project", seed=1, side_channels=[channel])
+	unity_env = UnityEnvironment(file_name="image_only_build_linux/ml-racing", seed=1, side_channels=[channel])
 	env = UnityToGymWrapper(unity_env, allow_multiple_obs=True)
-	env = FeatureExtractionWrapper(env)
+	env = ImageWrapper(env)
 	env = Monitor(env, config.log_dir)
 
 	reward_list = []
@@ -33,7 +34,6 @@ if __name__ == '__main__':
 	for i in range(5):
 		while True:
 			action, _states = model.predict(observation)
-			print("action: ", action)
 			observation, reward, done, info = env.step(action)	
 			commulative_reward += reward
 			# print("observation: ", observation)
