4192de0659e50e81f633897ce8f7471bfe970a4d
Ben Barkow <benbarkow02@gmail.com>
2023-08-29 14:01:35 +0200

    discrete

--- ML/evaluate.py
+++ ML/evaluate.py
@@ -20,13 +20,15 @@ from time import sleep
 parser = argparse.ArgumentParser(description='Train an RL agent. Can be used to train a new agent or to continue training an existing one.')
 parser.add_argument('-ex', '--executable', type=str, default="build/ml-racing-project", help='Executable to train on')
 parser.add_argument('-m', '--model', type=str, default="best_model.zip", help='Model to use')
+#timescale
+parser.add_argument('-ts', '--timescale', type=float, default=1.0, help='Time scale of the simulation')
 argus = parser.parse_args()
 
 if __name__ == '__main__':
 
     #init and start simulation
 	channel = EngineConfigurationChannel()
-	channel.set_configuration_parameters(time_scale=1.0)
+	channel.set_configuration_parameters(time_scale=argus.timescale)
 	unity_env = UnityEnvironment(file_name=argus.executable, seed=1, side_channels=[channel])
 	env = UnityToGymWrapper(unity_env, allow_multiple_obs=True)
 	env = ImageWrapper(env)
@@ -35,10 +37,16 @@ if __name__ == '__main__':
 	reward_list = []
 
 	model = PPO.load(config.models_dir + argus.model, env, device="cuda")
+
+	# Evaluate the agent
+	# mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=1)
+	# print("mean_reward: ", mean_reward, "std_reward: ", std_reward)
+
+
 	commulative_reward = 0
 	
 	observation = env.reset()
-	for i in range(5):
+	for i in range(30):
 		while True:
 			action, _states = model.predict(observation)
 			observation, reward, done, info = env.step(action)	
